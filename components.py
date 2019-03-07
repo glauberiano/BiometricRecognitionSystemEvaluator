dab8edd61626785ebe52cdeebdec4e516ab79edc
lucas <lucasmazim@yahoo.com.br>
2019-03-06 21:43:13 -0300

    atualizando classe de enrollment e classes para criar o fluxo de dados

--- components.py
+++ components.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
+import random
 from sklearn.metrics import classification_report, confusion_matrix 
 from abc import ABC, abstractmethod
 from classifiers import M2005
 
-class Cadastramento:
+class Enrollment:
     def __init__(self):
         pass
 
@@ -28,131 +30,83 @@ class Cadastramento:
         data_not_used = dataset.drop(dataset.index[index_used_in_db]) #salvar as observações que não foram utilizadas
         return user_information, data_not_used
     
-class DataStream:
-    '''
-    Class que cria o fluxo de dados utilizado para testes.
-    '''
-    def __init__(self, impostor_rate=0.5, rate_external_impostor=0.5):
-        self.__impostor_rate = impostor_rate # taxa de impostores
-        self.__rate_external_impostor = rate_external_impostor # dos impostores no fluxo, define quantos são externos
-    
-    def create(self, data=None, genuine=None, internal=None, external=None):
-        '''
-        parameters:
-            - data (pandas.Dataframe): amostras para serem usadas no fluxo de dados biométricos
-            - genuine (str): id do usuário genuino
-            - internal (list): id dos usuários registrados
-            - external (list): id dos usuários nao registrados
-        
-        return:
-            - dataStream (pandas.Dataframe): fluxo de dados biométricos
-        '''
+class DataStream(ABC):
+    def __init__(self, impostor_rate, rate_external_impostor):
+        self.impostor_rate = impostor_rate # taxa de impostores
+        self.rate_external_impostor = rate_external_impostor # dos impostores no fluxo, define quantos são externos
+        super().__init__()
+
+    def _extract_datasets(self, data, genuine, internal, external):
         genuine_samples = data[data['subject']==genuine] # separando amostras genuínas;
         in_samples = data[data['subject'].isin(internal)] # separando amostras de usuários registrados
         int_imp_samples = in_samples[in_samples['subject'] != genuine] # com exceção do usuário genuino;
         ext_imp_samples = data[data['subject'].isin(external)] # separando amostras de usuários não registrados
         
         # separando o número de amostras que serão utilizadas no fluxo
-        n_impostor = int((len(genuine_samples) * (1-self.__impostor_rate)) / self.__impostor_rate)
-        n_internal_imp = int(n_impostor * (1-self.__rate_external_impostor))
+        n_impostor = int((len(genuine_samples) * (1-self.impostor_rate)) / self.impostor_rate)
+        n_internal_imp = int(n_impostor * (1-self.rate_external_impostor))
         n_external_imp = n_impostor - n_internal_imp
-        
-        # enviando amostras aleatórias de cada usuário para a criação do fluxo
-        dataStream = Sampling.random(genuine_samples=genuine_samples,
-                                    internal_samples=int_imp_samples.iloc[:n_internal_imp],
-                                    external_samples=ext_imp_samples.iloc[:n_external_imp])
-        return dataStream
 
-class Sampling:
-    '''
-    Class que reproduz a forma de amostragem do fluxo de dados
-    '''
-    def __init__(self):
+        genuine_samples=genuine_samples
+        internal_samples=int_imp_samples.iloc[:n_internal_imp]
+        external_samples=ext_imp_samples.iloc[:n_external_imp]
+
+        return genuine_samples, internal_samples, external_samples
+        
+    def _extrai(self, df1):
+        a = df1.values.tolist()[0]
+        df1.drop(df1.index[0], inplace=True)
+        return a
+
+    @abstractmethod
+    def create(self):
         pass
 
-    def random(self, genuine_samples=None, internal_samples=None, external_samples=None):
-        '''
-        parameters:
-            - Recebe amostras genuínas e impostoras
-        return:
-            - dataStream
-        '''
-        d = [genuine_samples, internal_samples, external_samples]
-        dataStream = pd.concat(d) #concatenando os dados
-        dataStream = dataStream.sample(frac=1, random_state=42).reset_index(drop=True) #reordenando aleatóriamente
-        return dataStream
+class Random(DataStream):
+    def create(self, data=None, genuine=None, internal=None, external=None):
+        genuine_samples, internal_samples, external_samples = self._extract_datasets(data=data, genuine=genuine, internal=internal, external=external)
+        l_ds = list()
+        if type(genuine_samples) != None:
+            b = np.zeros(len(genuine_samples)).tolist()
+            l_ds.extend(b)
+            c = genuine_samples.columns
+            genuine_samples = genuine_samples.reset_index(drop=True)
+        if type(internal_samples) != None:
+            b = np.ones(len(internal_samples)).tolist()
+            l_ds.extend(b)
+            c = internal_samples.columns
+            internal_samples = internal_samples.reset_index(drop=True)
+        if type(external_samples) != None:
+            b = np.ones(len(external_samples))*2
+            l_ds.extend(b.tolist())
+            c = external_samples.columns
+            external_samples = external_samples.reset_index(drop=True)
 
-    
-    
-'''
-class classification():
-    def M2005_FE(dataset=None, ignored_columns=None):
-        features = dict()
-        for user in dataset.keys():
-            usft = dict()
-            data = dataset[user]
-            try:
-                data=data.drop(ignored_columns, axis=1)
-            except:
-                pass
-            for feature in data:
-                lower = min(data[feature].mean(), data[feature].median()) * (0.95 - (data[feature].std() / data[feature].mean()))
-                upper = max(data[feature].mean(), data[feature].median()) * (1.05 - (data[feature].std() / data[feature].mean()))
-                usft[feature] = (lower, upper)
-            features[user] = usft
-        return features
-    
-    def M2005(genuineUser=None, dataStream=None, biometricsDatabase=None, decision_threshold=0.00):
-        y_true = list()
-        y_pred = list()
-        list_of_scores = list()
-        
-        for index, row in dataStream.iterrows():
-            if (row['subject'] == genuineUser):
-                y_true.append('genuine')
-            else:
-                y_true.append('impostor')
-            match_sum = 0
-            previousDimMatched = False
-            for dim in biometricsDatabase[genuineUser].keys():
-                if (row[dim] < biometricsDatabase[genuineUser][dim][1]) & (row[dim] > biometricsDatabase[genuineUser][dim][0]):
-                    if previousDimMatched:
-                        match_sum = match_sum + 1.5
-                    else:
-                        match_sim = match_sum + 1.0
-                    previousDimMatched = True
-                else:
-                    previousDimMatched = False
-            max_sum = 1.0 + 1.5 * (len(biometricsDatabase[genuineUser].keys()) -1)
-            score = match_sum/max_sum
-            if score > decision_threshold:
-                y_pred.append('genuine')
-            else:
-                y_pred.append('impostor')
-            list_of_scores.append(score)
-        FNMR, FMR, B_acc = classification.report_metrics(y_true=y_true, y_pred=y_pred)
-        return FNMR, FMR, B_acc, list_of_scores
-'''
+        #import pdb; pdb.set_trace();
+        random.Random(42).shuffle(l_ds)
+        datastream = list()
+        for i in l_ds:
+            if i == 0:
+                datastream.append(self._extrai(genuine_samples))
+                genuine_samples = genuine_samples.reset_index(drop=True)
+            if i == 1:
+                datastream.append(self._extrai(internal_samples))
+                internal_samples = internal_samples.reset_index(drop=True)
+            if i == 2:
+                datastream.append(self._extrai(external_samples))
+                external_samples = external_samples.reset_index(drop=True)
 
-'''        
-class FeatureExtractor():
-    #Classe que extrai características (traços biométricos) de um conjunto de variáveis.
-    
-    def __init__(self):
-        pass
-    
-    def create_feat(self, dataset=None, method='M2005', ignored_columns=None):
-        
-        #parameters:
-        #    - dataset (pandas.Dataframe): banco de dados com informações de usuários
-        #    - method (str): método de extração
-        #    - ignored_columns (list): colunas do dataset que devem ser ignoradas
-        #
-        #return:
-        #    - banco de dados (dict) de traços biométricos de cada usuário registrado 
-        
-        biometricFeatures = dict() 
-        if method=='M2005':
-            biometricFeatures = classification.M2005_FE(dataset=dataset, ignored_columns=ignored_columns)
-            return biometricFeatures
-'''
+        datastream = pd.DataFrame(datastream, columns=c)
+        return datastream
+
+class GenFirst(DataStream):
+    def create(self, data=None, genuine=None, internal=None, external=None):
+        genuine_samples, internal_samples, external_samples = self._extract_datasets(data=data, genuine=genuine, internal=internal, external=external)
+        frames = [genuine_samples, internal_samples, external_samples]
+        return pd.concat(frames)
+
+class ImpFirst(DataStream):
+    def create(self, data=None, genuine=None, internal=None, external=None):
+        genuine_samples, internal_samples, external_samples = self._extract_datasets(data=data, genuine=genuine, internal=internal, external=external)
+        frames = [internal_samples, external_samples, genuine_samples]
+        return pd.concat(frames)
